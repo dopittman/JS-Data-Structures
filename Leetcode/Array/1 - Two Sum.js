0050990747b2b33e6a344bf41955ec3cff70cacf
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-04-25 20:39:59 -0400

    Include explanation for each part of the solution

--- Leetcode/Array/1 - Two Sum.js
+++ Leetcode/Array/1 - Two Sum.js
@@ -1,16 +1,24 @@
 var twoSum = function(nums, target) {
+    // Create map to hold possible solutions to our problem
     let list = new Map()
     
+    // Loop through given array
     for(let i = 0; i < nums.length; i++){
+        // Solution is the complement of the current 'nums' element and the target number
         let solution = target - nums[i];
+        
+        // If the Map contains the solution variable as a key, 
+        // return the saved index value of the first number
         if (list.has(solution)){
             return [list.get(solution), i]
         } else {
+            // Otherwise crate a key-value pair of currentElement: currentIndex
             list.set(nums[i], i)
         }
     }  
 };
 
+// Testing purposes only
 let testArr = [2,7,11,15]
 let target = 9
 
