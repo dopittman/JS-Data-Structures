42c556221d2a044ea1fc8393215c15c4c5b6fb1a
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-05-20 12:57:21 -0400

    Update with solution

--- Leetcode/Array/238 - Product of Array Except Self.js
+++ Leetcode/Array/238 - Product of Array Except Self.js
@@ -12,6 +12,7 @@ Input: nums = [1,2,3,4]
 Output: [24,12,8,6]
 Example 2:
 
+Example 2:
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
  
@@ -32,6 +33,26 @@ Follow up: Can you solve the problem in O(1) extra space complexity? (The output
  * @return {number[]}
  */
 
+const arrOne = [1,2,3,4]
+
  var productExceptSelf = function(nums) {
-    
+    let finalArr = []
+    let prefix = 1
+    let postfix = 1
+
+    // Perform prefix sum
+    for(let i = 0; i < nums.length; i++){
+        finalArr[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    // Perform postfix sum
+    for(let i = arrOne.length - 1; i >= 0; i--){
+        finalArr[i] = postfix * finalArr[i]
+        postfix *= nums[i]
+    }
+
+    return finalArr
 };
+
+console.log(productExceptSelf(arrOne));
