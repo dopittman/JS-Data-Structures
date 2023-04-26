a1fdeb45e8baa1333df3c4fce80c8727754d3d8a
David Pittman <53283971+dopittman@users.noreply.github.com>
2023-04-26 07:27:32 -0400

    solution: 35 - Search Insert

---
+++ Leetcode/Array/35 - Search Insert.js
@@ -0,0 +1,35 @@
+/* 
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+*/
+
+var searchInsert = function(nums, target) {
+    let l = 0
+    let r = nums.length
+
+    while(l < r) {
+        let mid = Math.floor(l + (r - l)/2)
+        if(nums[mid] === target){
+            return mid
+        } else if(nums[mid] < target){
+            l = mid  + 1
+        } else {
+            r = mid
+        }
+    } return r
+};
